7265dc34fbb0ace450e6deaf18a41994ae8bec53
Volodia <storozhenko.vladimir.vasilhevich@gmail.com>
2020-04-02 21:47:19 +0300

    fix: es-lint warnings detected

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 (function KEYBOARD() {
-  const ENGLISH = ['§ ±', '1 !', '2 @', '3 #', '4 $', '5 %', '6 ˆ', '7 &', '8 *', '9 (', '0 )', '- _', '= +', 'Backspace', '',
-    'Tab', 'q Q', 'w W', 'e E', 'r R', 't T', 'y Y', 'u U', 'i I', 'o O', 'p P', '[ {', '] }', '',
-    'CapsLock', 'a A', 's S', 'd D', 'f F', 'g G', 'h H', 'j J', 'k K', 'l L', '; :', '\' "', '\\ |', '',
-    'Shift', '` ˜', 'z Z', 'x X', 'c C', 'v V', 'b B', 'n N', 'm M', ', <', '. >', '/ ?', 'Enter', '',
-    'Control', 'Alt', 'Meta', 'Space'];
+  const ENGLISH = ['§ ±', '1 !', '2 @', '3 #', '4 $', '5 %', '6 ˆ', '7 &', '8 *', '9 (', '0 )', '- _', '= +', 'Backspace', '😍', '😂', '😎', '',
+    'Tab', 'q Q', 'w W', 'e E', 'r R', 't T', 'y Y', 'u U', 'i I', 'o O', 'p P', '[ {', '] }', '🤣', '😊', '🤩', '',
+    'CapsLock', 'a A', 's S', 'd D', 'f F', 'g G', 'h H', 'j J', 'k K', 'l L', '; :', '\' "', '\\ |', '😭', '✌', '☝', '',
+    'Shift', '` ˜', 'z Z', 'x X', 'c C', 'v V', 'b B', 'n N', 'm M', ', <', '. >', '/ ?', 'Enter', 'STUB', 'ArrowUp', '',
+    'Control', 'Alt', 'Meta', 'Space', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
-  const RUSSIAN = ['ё Ё', '1 !', '2 "', '3 №', '4 ❤️️', '5 %', '6 :', '7 ?', '8 *', '9 (', '0 )', '- _', '= +', 'Backspace', '',
-    'Tab', 'й Й', 'ц Ц', 'у У', 'к К', 'е Е', 'н Н', 'г Г', 'ш Ш', 'щ Щ', 'з З', 'х Х', 'ъ Ъ', '',
-    'CapsLock', 'ф Ф', 'ы Ы', 'в В', 'а А', 'п П', 'р Р', 'о О', 'л Л', 'д Д', 'ж Ж', 'э Э', '® ©', '',
-    'Shift', '¥ £', 'я Я', 'ч Ч', 'с С', 'м М', 'и И', 'т Т', 'ь Ь', 'б Б', 'ю Ю', '« »', 'Enter', '',
-    'Control', 'Alt', 'Meta', 'Space'];
+  const RUSSIAN = ['ё Ё', '1 !', '2 "', '3 №', '4 ❤️️', '5 %', '6 :', '7 ?', '8 *', '9 (', '0 )', '- _', '= +', 'Backspace', '😍', '😂', '😎', '',
+    'Tab', 'й Й', 'ц Ц', 'у У', 'к К', 'е Е', 'н Н', 'г Г', 'ш Ш', 'щ Щ', 'з З', 'х Х', 'ъ Ъ', '🤣', '😊', '🤩', '',
+    'CapsLock', 'ф Ф', 'ы Ы', 'в В', 'а А', 'п П', 'р Р', 'о О', 'л Л', 'д Д', 'ж Ж', 'э Э', '® ©', '😭', '✌', '☝', '',
+    'Shift', '¥ £', 'я Я', 'ч Ч', 'с С', 'м М', 'и И', 'т Т', 'ь Ь', 'б Б', 'ю Ю', '« »', 'Enter', 'STUB', 'ArrowUp', '',
+    'Control', 'Alt', 'Meta', 'Space', 'ArrowLeft', 'ArrowDown', 'ArrowRight'];
 
   let characters; // simbols keyboard
   let letters;
-  const commands = [];
+  const btnCombination = new Set();
   const textArea = document.createElement('textarea');
   textArea.disabled = true; // block mouse focus and direct input
   textArea.id = 'text-area';
 
   const toggleHidden = (element) => element.classList.toggle('hidden');
-  const changeLanguage = () => {
-    [...document.querySelectorAll('.keyboard')].forEach(toggleHidden);
-    localStorage.language = (localStorage.language === 'english') ? 'russian' : 'english';
+
+  // switch keyboard language
+  const langChangeHandler = (event) => {
+    const a = [...btnCombination];
+    if (a.every((i) => event[i])) {
+      [...document.querySelectorAll('.keyboard')].forEach(toggleHidden);
+      localStorage.language = (localStorage.language === 'english') ? 'russian' : 'english';
+    }
+  };
+
+  const detectCombination = (event) => {
+    if (event.shiftKey) { btnCombination.add('shiftKey'); }
+    if (event.ctrlKey) { btnCombination.add('ctrlKey'); }
+    if (event.altKey) { btnCombination.add('altKey'); }
+    if (event.metaKey) { btnCombination.add('metaKey'); }
+    if (event.code === 'Space') { btnCombination.add('space'); }
+  };
+
+  const setCombination = () => {
+    if (btnCombination.size < 2) { return; }
+    setTimeout(() => {
+      const combination = document.querySelector('.combination');
+      combination.innerHTML = [...btnCombination].join(' + ');
+      document.querySelector('.description').style.borderLeftColor = 'green';
+      document.removeEventListener('keydown', detectCombination);
+      document.addEventListener('keydown', langChangeHandler);
+    }, 500);
+    setTimeout(() => { document.querySelector('.request').style.opacity = '0'; }, 1000);
   };
 
   // return string with type of button
@@ -29,17 +54,20 @@
     const currBtn = button.toString();
     const alphabet = 'qwertyuiopasdfghjklzxcvbnmёйцукенгшщзхъфывапролджэячсмитьбю'.split('');
     const controls = ['CapsLock', 'Shift', 'Ctrl', 'Meta', 'Alt', 'Control'];
+    const arrows = ['ArrowLeft', 'ArrowDown', 'ArrowRight', 'ArrowUp'];
 
     if (currBtn === '') { return 'new line'; }
     if (controls.includes(currBtn)) { return 'command'; }
     if (alphabet.includes(currBtn.toLowerCase())) { return 'letter'; }
+    if (arrows.includes(currBtn)) { return 'arrow'; }
+
     return 'simbol';
   };
 
   // return SpanHTMLElement (key button) //
   const createKey = (char) => {
     if (keyType(char) === 'new line') {
-      return document.createElement('div');
+      return document.createElement('br');
     }
     const span = document.createElement('span');
     span.innerHTML = char;
@@ -67,6 +95,7 @@
 
   const print = (key) => {
     const textarea = document.querySelector('textarea');
+    const elem = document.querySelector(`[id='${key}']`);
     switch (key) {
       case 'Backspace':
         textarea.value = textarea.value.slice(0, -1);
@@ -81,14 +110,17 @@
         textarea.value += ' ';
         break;
       default:
-        if (key.length < 2) { // press CHOISING language button
+        if (key.length <= 2) { // letters, simbols, arrows
           const pressedBtn = document.querySelector('.keyboard:not(.hidden) .pressed:not(.hidden):not(.command)');
-          textarea.value += (pressedBtn) ? pressedBtn.id : key;
+          textarea.value += (pressedBtn) ? pressedBtn.id : key; // for simbols
+        }
+        if (elem.classList.contains('arrow')) {
+          textarea.value += elem.innerHTML;
         }
     }
   };
 
-  // DANGEROUS !!! BUTTHURT !!!
+  // adds a class to a pressed key on ALL layouts
   const togglePressed = (event, add = false) => {
     let pressed = [...ENGLISH, ...RUSSIAN].find((i) => i.split(' ').includes(event.key)); // find pressed keys in layout
 
@@ -97,15 +129,16 @@
     } else {
       pressed = [pressed, ENGLISH[RUSSIAN.indexOf(pressed)]];
     }
+    // DANGEROUS !!! BUTTHURT !!!
     pressed.filter((i) => i).join(' ').split(' ').forEach((item) => {
       const elems = document.querySelectorAll(`[id='${item}']`);
       elems.forEach((elem) => ((add) ? elem.classList.add('pressed') : elem.classList.remove('pressed')));
     });
   };
 
-  const keydownHandler = (event) => { // keyboard event handler
-    // avoid focus by TAB button
-    if (event.key === 'Tab') { event.preventDefault(); }
+  // keyboard events handlers
+  const keydownHandler = (event) => {
+    if (event.key === 'Tab') { event.preventDefault(); } // avoid focus by TAB button
     if (event.key === 'Shift') { [...characters, ...letters].forEach(toggleHidden); }
     if (event.code === 'CapsLock') { letters.forEach(toggleHidden); }
     if (event.code === 'Space') { // find element not by 'event.code' == "Space"
@@ -113,7 +146,6 @@
     } else { togglePressed(event, true); }
     print(event.key); // add .key to textarea
   };
-
   const keyupHandler = (event) => {
     if (event.code === 'CapsLock') { letters.forEach(toggleHidden); }
     if (event.key === 'Shift') { [...characters, ...letters].forEach(toggleHidden); }
@@ -122,65 +154,62 @@
     } else { togglePressed(event); }
   };
 
+  // mouse events handlers
   const mousedownHandler = (event) => {
     if (event.target.tagName !== 'SPAN') { return; }
     event.target.classList.toggle('pressed');
-    if (event.target.id === 'CapsLock') { letters.forEach(toggleHidden); }
-    if (event.target.id === 'Shift') { [...characters, ...letters].forEach(toggleHidden); }
-    print(event.target.id);
+    if (event.target.id === 'CapsLock') { letters.forEach(toggleHidden); } // toggle letters
+    if (event.target.id === 'Shift') { [...characters, ...letters].forEach(toggleHidden); } // toggle letters and simbols
+    print(event.target.id); // add to textarea
   };
 
   const mouseupHandler = (event) => {
     if (event.target.tagName !== 'SPAN') { return; }
-    if (!event.target.classList.contains('command')) {
+    if (!event.target.classList.contains('command')) { // commands butons still pressed
       event.target.classList.toggle('pressed');
     }
+  };
+
+  const createReadme = () => {
+    const readme = document.createElement('div');
+    const request = document.createElement('p');
+    const description = document.createElement('p');
+    const combinationEl = document.createElement('code');
+
+    readme.classList.add('loader');
+    request.classList.add('request');
+    description.classList.add('description');
+    description.innerText = 'Your combination:';
+    combinationEl.classList.add('combination');
+    request.innerText = 'Please, press buttons combination to switch keyboard layout...';
+    description.append(combinationEl);
+    readme.append(request, description);
+
+    return readme;
   };
 
   // create markup and set keyboard layout //
   const engKeyboard = createKeyboard(ENGLISH);
   const ruKeyboard = createKeyboard(RUSSIAN);
+  const readme = createReadme();
+  // hide all layouts
   engKeyboard.classList.add('english', 'hidden');
   ruKeyboard.classList.add('russian', 'hidden');
-  document.body.append(textArea, engKeyboard, ruKeyboard);
+  document.body.append(readme, textArea, engKeyboard, ruKeyboard);
 
   if (!localStorage.getItem('language')) {
     localStorage.setItem('language', 'english');
   }
+  // show selected layout
   document.querySelector(`.${localStorage.getItem('language')}`).classList.remove('hidden');
 
-  const btnCombination = new Set();
-  const combination = document.querySelector('.combination');
-  function setBtnCombination(event) {
-    if (event.shiftKey) { btnCombination.add('shiftKey'); }
-    if (event.ctrlKey) { btnCombination.add('ctrlKey'); }
-    if (event.altKey) { btnCombination.add('altKey'); }
-    if (event.metaKey) { btnCombination.add('metaKey'); }
-    if (event.code === 'Space') { btnCombination.add('space'); }
-  }
-
-  const langChangeHandler = (e) => {
-    const a = [...btnCombination];
-    if (a.every((i) => e[i])) changeLanguage();
-  };
-
-  document.addEventListener('keydown', setBtnCombination);
-
-  document.addEventListener('keydown', () => {
-    if (btnCombination.size < 2) { return; }
-    setTimeout(() => {
-      combination.innerHTML = [...btnCombination].join(' + ');
-      document.querySelector('.description').style.borderLeftColor = 'green';
-      document.removeEventListener('keydown', setBtnCombination);
-      document.addEventListener('keydown', langChangeHandler);
-    }, 500);
-    setTimeout(() => { document.querySelector('.request').style.opacity = '0'; }, 1000);
-  });
+  // set buttons combination to switch languages
+  document.addEventListener('keydown', detectCombination);
+  document.addEventListener('keydown', setCombination);
 
   // nodes arrays by behavior
   characters = [...document.getElementsByClassName('simbol')];
   letters = [...document.getElementsByClassName('letter')];
-  commands.concat(document.getElementsByClassName('command'));
 
   // keyboard events listeners
   document.addEventListener('keydown', keydownHandler);
@@ -188,4 +217,10 @@
   // mouse events listeners
   document.addEventListener('mousedown', mousedownHandler);
   document.addEventListener('mouseup', mouseupHandler);
+
+  // arrows content
+  document.querySelectorAll('#ArrowUp').forEach((i) => { const j = i; j.innerHTML = '⇧'; });
+  document.querySelectorAll('#ArrowDown').forEach((i) => { const j = i; j.innerHTML = '⇩'; });
+  document.querySelectorAll('#ArrowRight').forEach((i) => { const j = i; j.innerHTML = '⇨'; });
+  document.querySelectorAll('#ArrowLeft').forEach((i) => { const j = i; j.innerHTML = '⇦'; });
 }());
