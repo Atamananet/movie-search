54b8308bdaf58499beb217e841d34ca794cfdb98
atamananet <storozhenko.vladimir.vasilhevich@gamil.com>
2020-04-27 10:35:21 +0300

    init: install swiper

--- movie-search/src/js/app.js
+++ movie-search/src/js/app.js
@@ -1,4 +1,11 @@
-import 'popper.js';
 import 'bootstrap';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Swiper from 'swiper';
+import 'swiper/css/swiper.min.css';
 import '../css/main.css';
+
+
+var mySwiper = new Swiper('.swiper-container', {
+    speed: 400,
+    spaceBetween: 100
+});
