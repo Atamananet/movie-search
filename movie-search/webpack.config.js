544ea5a9887ed8cb5002a6703bca73c6eb8fae5f
atamananet <storozhenko.vladimir.vasilhevich@gamil.com>
2020-04-27 08:56:17 +0300

    init: install, config webpack

---
+++ movie-search/webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/js/app.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+        exclude: /\.module\.css$/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }
+        ],
+        include: /\.module\.css$/
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = config;
